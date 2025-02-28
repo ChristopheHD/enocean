da96a3f412bb4e73f00d52a246e104159c32bbca
Christophe Hardouin Duparc <christophe.hardouin.duparc@gmail.com>
2025-02-28 15:51:47 +0100

    Update eep.py

--- enocean/protocol/eep.py
+++ enocean/protocol/eep.py
@@ -22,10 +22,10 @@ class EEP(object):
         try:
             if version_info[0] > 2:
                 with open(eep_path, 'r', encoding='UTF-8') as xml_file:
-                    self.soup = BeautifulSoup(xml_file.read(), "lxml")
+                    self.soup = BeautifulSoup(xml_file.read(), "lxml-xml")
             else:
                 with open(eep_path, 'r') as xml_file:
-                    self.soup = BeautifulSoup(xml_file.read(), "lxml")
+                    self.soup = BeautifulSoup(xml_file.read(), "lxml-xml")
             self.init_ok = True
             self.__load_xml()
         except IOError:
