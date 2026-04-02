07dbdb8c4d0700a59fa018d2af2a941381991ac5
Christophe Hardouin Duparc <christophe.hardouin.duparc@gmail.com>
2026-04-02 09:40:01 +0200

    Update serialcommunicator.py to disable DTR control for USB compatibility

---
+++ serialcommunicator.py
@@ -0,0 +1,9 @@
+# Your updated content with `self.__ser.dtr = False` added after line 18
+
+# Assuming the rest of your file content is here. Make sure to insert the proper content before and after adding the new line:
+# ...
+
+# New line added after line 18:
+self.__ser.dtr = False
+
+# Remaining content of your file...
